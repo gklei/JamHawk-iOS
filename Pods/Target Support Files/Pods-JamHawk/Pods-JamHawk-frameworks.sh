ce665b9ec8f09798f713c346080e933ae626eddb
gklei <gregoryhklein@gmail.com>
2016-07-10 21:24:33 -0400

    Added FDWaveformView pod

--- Pods/Target Support Files/Pods-JamHawk/Pods-JamHawk-frameworks.sh
+++ Pods/Target Support Files/Pods-JamHawk/Pods-JamHawk-frameworks.sh
@@ -85,11 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AsyncImageView/AsyncImageView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FDWaveformView/FDWaveformView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Freddy/Freddy.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IncipiaKit/IncipiaKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AsyncImageView/AsyncImageView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FDWaveformView/FDWaveformView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Freddy/Freddy.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IncipiaKit/IncipiaKit.framework"
 fi
