5e9277b32844c293a8be9c563b6625d0b3566a0e
Brendan Lau <bjlau@umich.edu>
2016-07-25 17:20:49 -0400

    Started Implementing ProfileView

--- JamHawk/ViewControllers/ProfileViewController.swift
+++ JamHawk/ViewControllers/ProfileViewController.swift
@@ -9,4 +9,53 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+	
+   @IBOutlet weak var _profileOptionsCollectionView: UICollectionView!
+   
+   
+	// MARK: - Overridden
+	override func viewDidLoad() {
+		super.viewDidLoad()
+      let layout = UICollectionViewFlowLayout()
+      layout.itemSize = CGSize(width: _profileOptionsCollectionView.bounds.width, height: 50)
+      layout.minimumLineSpacing = 1.0
+      layout.scrollDirection = .Vertical
+      
+      _profileOptionsCollectionView.dataSource = self
+      _profileOptionsCollectionView.delegate = self
+      
+      _profileOptionsCollectionView.collectionViewLayout = layout
+	}
+	
+   
+   
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		removeLeftBarItem()
+	}
+}
+
+extension ProfileViewController: UICollectionViewDataSource {
+   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      return 2
+   }
+   
+   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+      let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ProfileOptionCell", forIndexPath: indexPath) as! ProfileOptionCell
+      cell.titleLabel.text = "Fuck"
+      return cell
+   }
+}
+
+extension ProfileViewController: UICollectionViewDelegate {
+}
+
+class ProfileOptionCell: UICollectionViewCell {
+   
+   @IBOutlet var titleLabel: UILabel!
+   
+   override func awakeFromNib() {
+      super.awakeFromNib()
+      backgroundColor = .redColor()
+   }
 }
