4a4effaf874ee7ce82ae00a9e1558d0fce943c6e
gklei <gregoryhklein@gmail.com>
2016-07-30 19:39:34 -0400

    WIP - getting the timer to trigger api requests at a set interval

--- JamHawk/Controllers/SystemCoordinationController.swift
+++ JamHawk/Controllers/SystemCoordinationController.swift
@@ -20,7 +20,10 @@ class SystemCoordinationController {
 	let nextAvailableSystem = NextAvailableMediaSystemController()
 	let ratingSystem = TrackRatingSystemController()
 	
+	private var _timer: NSTimer?
 	private let _playerAPIService: PlayerAPIService
+	
+	private var _subfilterIDsSinceLastRequest: [PlayerAPIFilterID] = []
 	
 	var errorPresentationContext: UIViewController?
 	
@@ -39,15 +42,21 @@ class SystemCoordinationController {
 		playerSystem.update(withModel: output.media)
 		filterSystem.update(withModel: output.filters)
 		currentTrackSystem.update(withModel: output.track)
-		nextAvailableSystem.update(withModel: output.next)
-		nextAvailableSystem.selectMedia(atIndex: 0)
+		
+		if output.next != nil {
+			nextAvailableSystem.update(withModel: output.next)
+			nextAvailableSystem.selectMedia(atIndex: 0)
+		}
+		
 		ratingSystem.update(withModel: output.track)
 	}
 	
 	func instantiatePlayer(completion: ((error: NSError?) -> Void)?) {
 		_playerAPIService.instantiatePlayer { (error, output) in
 			self._handlePlayerAPICallback(error, output: output)
+			
 			completion?(error: error)
+			self._setupRequestTimer(withInterval: 3)
 		}
 	}
 }
@@ -58,5 +67,38 @@ extension SystemCoordinationController: PlayerSystemDelegate {
 		
 		let updates = PlayerAPIInputUpdates(abandonedRequests: nil, canPlay: true, filter: nil, select: next.mid, ratings: nil)
 		_playerAPIService.requestNextTrack(withUpdates: updates, callback: _handlePlayerAPICallback)
+	}
+}
+
+extension SystemCoordinationController {
+	
+	private func _setupRequestTimer(withInterval seconds: NSTimeInterval) {
+		guard _timer == nil else { return }
+		
+		let fireDate = NSDate(timeIntervalSinceNow: 3)
+		let selector = #selector(_sendRequestToPlayerAPI(_:))
+		_timer = NSTimer(fireDate: fireDate, interval: seconds, target: self, selector: selector, userInfo: nil, repeats: true)
+		NSRunLoop.mainRunLoop().addTimer(_timer!, forMode: NSDefaultRunLoopMode)
+	}
+	
+	@objc internal func _sendRequestToPlayerAPI(timer: NSTimer) {
+		let selection = _generateFilterSelectionIfChanged()
+		let next = nextAvailableSystem.currentNextTrackSelection?.mid
+		let updates = PlayerAPIInputUpdates(abandonedRequests: nil, canPlay: true, filter: selection, select: next, ratings: nil)
+		_playerAPIService.sendRequest(needNext: (selection != nil),
+		                              needMedia: false,
+		                              needFilters: false,
+		                              updates: updates,
+		                              callback: _handlePlayerAPICallback)
+	}
+	
+	private func _generateFilterSelectionIfChanged() -> PlayerAPIInputFilterSelection? {
+		var selection: PlayerAPIInputFilterSelection?
+		let selectedSubfilterIDs = filterSystem.selectedSubfilterIDs
+		if _subfilterIDsSinceLastRequest != selectedSubfilterIDs {
+			_subfilterIDsSinceLastRequest = selectedSubfilterIDs
+			selection = PlayerAPIInputFilterSelection(selection: filterSystem.filterSelection)
+		}
+		return selection
 	}
 }
