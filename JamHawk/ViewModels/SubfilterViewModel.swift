2814578662ee4144d1b5913aa45b78ec0edbec0d
gklei <gregoryhklein@gmail.com>
2016-07-30 19:39:05 -0400

    Moved SubfilterViewModel implementation into its own file

--- JamHawk/ViewModels/SubfilterViewModel.swift
+++ JamHawk/ViewModels/SubfilterViewModel.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct SubfilterViewModel: Equatable {
+	let category: String
+	let name: String
+	let id: String
+}
+
+func ==(lhs: SubfilterViewModel, rhs: SubfilterViewModel) -> Bool {
+	return lhs.category == rhs.category && lhs.id == rhs.id
+}
