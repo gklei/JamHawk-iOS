6bbfd1b5521c086bbe3d07e24d253d6171c9c7b4
gklei <gregoryhklein@gmail.com>
2016-08-01 11:37:57 -0400

    Slightly reduced the blur and the saturation delta factor

--- JamHawk/ViewControllers/MainPlayerViewController.swift
+++ JamHawk/ViewControllers/MainPlayerViewController.swift
@@ -144,7 +144,7 @@ final class MainPlayerViewController: UIViewController, PlayerStoryboardInstanti
 // MARK: - Async Image Downloading
 extension MainPlayerViewController {
 	internal func _imageFinishedLoading(image: UIImage?, url: NSURL?) {
-		_backgroundImageView.image = image?.applyBlur(withRadius: 3.5, tintColor: nil, saturationDeltaFactor: 1.2)
+		_backgroundImageView.image = image?.applyBlur(withRadius: 2.5, tintColor: nil, saturationDeltaFactor: 1.2)
 	}
 }
 
